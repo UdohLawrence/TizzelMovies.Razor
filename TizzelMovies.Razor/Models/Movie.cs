1cc32721f0da8bc43cb98e8c50c0ad6f83828b43
UdohLawrence <lawrenceaudoh@gmail.com>
2025-08-13 18:40:58 +0100

    project completion

--- TizzelMovies.Razor/Models/Movie.cs
+++ TizzelMovies.Razor/Models/Movie.cs
@@ -1,14 +1,32 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace TizzelMovies.Razor.Models
 {
     public class Movie
     {
         public int Id { get; set; }
-        public string? Title { get; set; }
+
+        [Required]
+        [StringLength(60, MinimumLength = 3)]
+        public string Title { get; set; } = string.Empty;
+
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
-        public string? Genre { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-z\s]*$")]
+        [StringLength(30)]
+        [Required]
+        public string Genre { get; set; } = string.Empty;
+
+        [Range(1,100)]
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18,2)")]
         public decimal Price { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-z0-9""'\s-]*$")]
+        [StringLength(5)]
+        [Required]
+        public string Rating { get; set; } = string.Empty;
     }
 }
---
+++ TizzelMovies.Razor/Models/SeedData.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TizzelMovies.Razor.Data;
+
+namespace TizzelMovies.Razor.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using var context = new TizzelMoviesRazorContext(
+                serviceProvider.GetRequiredService<DbContextOptions<TizzelMoviesRazorContext>>());
+            if(context == null || context.Movie == null)
+            {
+                throw new ArgumentNullException("Null TizzelMoviesRazorContext");
+            }
+            // Look for any movies.
+            //if (context.Movie.Any())
+            //{
+            //    return;   // DB has been seeded
+            //}
+            context.Movie.AddRange(
+                new Movie
+                {
+                    Title = "Inception",
+                    ReleaseDate = DateTime.Parse("2010-7-16"),
+                    Genre = "Science Fiction",
+                    Price = 9.99M,
+                    Rating = "R"
+                },
+                new Movie
+                {
+                    Title = "The Godfather",
+                    ReleaseDate = DateTime.Parse("1972-3-24"),
+                    Genre = "Crime",
+                    Price = 12.99M,
+                    Rating = "G"
+                },
+                new Movie
+                {
+                    Title = "The Dark Knight",
+                    ReleaseDate = DateTime.Parse("2008-7-18"),
+                    Genre = "Action",
+                    Price = 14.99M,
+                    Rating = "R"
+                },
+                new Movie
+                {
+                    Title = "When Harry met Sally",
+                    ReleaseDate = DateTime.Parse("2008-7-18"),
+                    Genre = "Romantic Comedy",
+                    Price = 14.99M,
+                    Rating = "R"
+                },
+                new Movie
+                {
+                    Title = "Prince of Persia: Sands of Time",
+                    ReleaseDate = DateTime.Parse("2008-7-18"),
+                    Genre = "Classic",
+                    Price = 1.94M,
+                    Rating = "M"
+                },
+                new Movie
+                {
+                    Title = "Pirates of the Carribean",
+                    ReleaseDate = DateTime.Parse("2008-7-18"),
+                    Genre = "Adventure",
+                    Price = 9.4M,
+                    Rating = "PG"
+                }
+
+            );
+            context.SaveChanges();
+        }
+    }
+}
